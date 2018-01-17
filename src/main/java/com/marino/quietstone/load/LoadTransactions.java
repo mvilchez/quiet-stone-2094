0e7b2c73f403b8b97baddd52873457bb6e62b901
marino.vilchez <marino.vilchez@gmail.com>
2018-01-17 21:47:44 +0100

    Modificado test

--- src/main/java/com/marino/quietstone/load/LoadTransactions.java
+++ src/main/java/com/marino/quietstone/load/LoadTransactions.java
@@ -1,16 +1,59 @@
 package com.marino.quietstone.load;
 
 import com.marino.quietstone.model.Transaction;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LoadTransactions {
     public List<Transaction> loadTransactions() {
-        String jsonData = LoadHelper.readFile("transactions.json");
-        JSONObject jobj = new JSONObject(jsonData);
-        JSONArray jarr = new JSONArray(jobj.getJSONArray("keywords").toString());
+        //JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader("transactions.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+
+            JSONArray employeeList = (JSONArray) obj;
+            System.out.println(employeeList);
+
+            //Iterate over employee array
+            employeeList.forEach( emp -> parseTransactionObject( (JSONObject) emp ) );
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         List<Transaction> transactions = new ArrayList<Transaction>();
         return transactions;
+    }
+
+    private static void parseTransactionObject(JSONObject employee)
+    {
+        //Get employee object within list
+        JSONObject employeeObject = (JSONObject) employee.get("employee");
+
+        //Get employee first name
+        String firstName = (String) employeeObject.get("firstName");
+        System.out.println(firstName);
+
+        //Get employee last name
+        String lastName = (String) employeeObject.get("lastName");
+        System.out.println(lastName);
+
+        //Get employee website name
+        String website = (String) employeeObject.get("website");
+        System.out.println(website);
     }
 }
